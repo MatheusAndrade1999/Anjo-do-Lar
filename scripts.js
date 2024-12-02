342b2556565d90b0eb5ab2659c01b9ba09ceae3a
MatheusAndrade1999 <matheusluiz194@gmail.com>
2024-12-01 22:33:25 -0300

    Sprint 4 finalizada, implementação de mapa, botão do whatsapp com link e formulario para envio de email, funcional, tudo em javascript

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Código para o Carrossel
 let currentIndex = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -18,3 +19,21 @@ function prevSlide() {
 
 // Navegar automaticamente a cada 5 segundos
 setInterval(nextSlide, 5000);
+
+
+// Código para Enviar o Formulário com EmailJS
+// Inicialize o EmailJS com a sua Private Key
+emailjs.init("8jaj7b3ql0v0j6qPb7ZSA");  // Substitua "sua_private_key" pela sua chave privada
+
+// Função para enviar o formulário
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();  // Evitar o envio tradicional do formulário
+
+  // Enviar o formulário com o EmailJS
+  emailjs.sendForm('service_35imb5h', 'template_rx2c5al', this)  // Substitua "service_id" e "template_id"
+    .then(function(response) {
+      alert('Mensagem enviada com sucesso!');
+    }, function(error) {
+      alert('Erro ao enviar mensagem. Tente novamente.');
+    });
+});
